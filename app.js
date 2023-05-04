6894c67964718679bfe3ff8640800c4f47d66d76
Akash Mane <113574567+Akashh-17@users.noreply.github.com>
2023-05-04 12:19:37 +0530

    Update app.js
    clean some code and added feature of change bg on the basis of temprature

--- app.js
+++ app.js
@@ -1,14 +1,52 @@
 let resutlt = document.getElementById("result");
 let searchBtn = document.getElementById("search-btn");
 let cityRef = document.getElementById("city");
+let body = document.querySelector("body");
 
-//Function to fectch weather details from api and display them
+// Displays background color based on temperature
+function setBackground(temp) {
+    console.log("Temp is ---:  " + temp);
+    if (temp > 30) {
+      body.style.backgroundColor = '#FF6000';
+    } else if (temp > 20 && temp <= 30) {
+      body.style.backgroundColor = '#FF6D60';
+    } else if (temp <= 20) {
+      body.style.backgroundColor = '#19A7CE';
+    }else {
+      body.style.backgroundColor = '#516395';
+    }
+}
+
+//displays output using the data from api
+function displayOutput(apiData) {
+  resutlt.innerHTML = `
+  <h2>${apiData.name}</h2>
+  <h4 class="weather">${apiData.weather[0].main}</h4>
+  <h4 class="desc">${apiData.weather[0].description}</h4>
+  <img src="https://openweathermap.org/img/w/${apiData.weather[0].icon}.png">
+  <h1>${apiData.main.temp} &#176;</h1>
+  <div class="temp-container">
+    <div>
+      <h4 class="title">min</h4>
+      <h4 class="temp">${apiData.main.temp_min}</h4>
+    </div>
+    <div>
+      <h4 class="title">max</h4>
+      <h4 class="temp">${apiData.main.temp_max}</h4>
+    </div>
+  </div>
+  `;
+  setBackground(apiData.main.temp);
+}
+
+//to fetch data from api and calls displayOutput function
 let getWeather = () => {
   let cityValue = cityRef.value;
 
   //If input field is empty
   if (cityValue == 0) {
     resutlt.innerHTML = `<h3 class="msg">Please enter a city name</h3>`;
+    body.style.backgroundColor = '#516395';
   }
   //If input field is NOT empty
   else {
@@ -18,40 +56,17 @@ let getWeather = () => {
     //Clear the input fiel
     cityRef.value = "";
 
-    fetch(url)
-    .then((resp) => resp.json())
-    // If city name is valid
-    .then((data) => {
-      console.log(data);
-      console.log(data.weather[0].icon);
-      console.log(data.weather[0].main);
-      console.log(data.weather[0].description);
-      console.log(data.name);
-      console.log(data.main.temp_min);
-      console.log(data.main.temp_max);
-
-      resutlt.innerHTML = `
-      <h2>${data.name}</h2>
-      <h4 class="weather">${data.weather[0].main}</h4>
-      <h4 class="desc">${data.weather[0].description}</h4>
-      <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
-      <h1>${data.main.temp} &#176;</h1>
-      <div class="temp-container">
-        <div>
-          <h4 class="title">min</h4>
-          <h4 class="temp">${data.main.temp_min}</h4>
-        </div>
-        <div>
-          <h4 class="title">max</h4>
-          <h4 class="temp">${data.main.temp_max}</h4>
-        </div>
-      </div>
-      `;
-    })
-    // If city name is not valid
-    .catch(() => {
-      resutlt.innerHTML = `<h3 class="msg">City not found</h3>`;
-    });
+    fetch(url).then((resp) => resp.json())
+      // If city name is valid
+      .then((data) => { 
+        //data object is pass there so that function can access data from api
+        displayOutput(data);
+      })
+      // If city name is not valid
+      .catch(() => {
+        resutlt.innerHTML = `<h3 class="msg">City not found</h3>`;
+        body.style.backgroundColor = '#516395';
+      });
   }
 };
 
